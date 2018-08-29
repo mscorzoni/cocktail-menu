fb3c0e9717081a8127de19942cb7fa87f6ef3efd
Marcos Scorzoni <mlessa@gmail.com>
2018-08-29 13:23:43 +0100

    setup doses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'doses/create'
   root to: 'cocktails#index'
-  resources :cocktails
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create, :new ]
+  end
+
+  resources :doses, only: [:destroy]
 end
