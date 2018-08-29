249e8245a786c90c4d9747bfe7cc904f5ba5baed
Marcos Scorzoni <mlessa@gmail.com>
2018-08-29 14:05:08 +0100

    add seed through API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients['drinks'].each do |ingredient|
+
+  Ingredient.create(name: ingredient['strIngredient1'])
+
+end
+
